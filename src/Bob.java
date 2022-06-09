0fe556ba4fededacabb7774348acb831356aff25
Marcus Munoz <munoz.marcus05@gmail.com>
2022-06-09 15:46:55 -0500

    both strings exercises completed

---
+++ src/Bob.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+//public class Bob {
+//    public static void main(String[] args){
+
+
+//        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//        He answers 'Whatever.' to anything else.
+//        Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Say something to Bob!");
+//        String userInput = sc.nextLine();
+//
+//        // ends with ?
+//        if (userInput.endsWith("?")) {
+//            System.out.println("Sure");
+//            // ends with !
+//        } else if (userInput.endsWith("!")) {
+//            System.out.println("Whoa, chill out!");
+//            // empty string
+//        } else if (userInput.trim().equals("")) {
+//            System.out.println("Fine; be that way.");
+//            // everything else
+//        } else {
+//            System.out.println("Whatever.");
+//        }
+//
+//    }
+//}
---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+//public class StringExercise {
+//    public static void main(String[] args){
+
+//        STRING BASICS
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console,
+//        you can do this with only one String variable and one print statement for each output example.
+
+//                System.out.println("We don't need no education\n" +
+//                "We don't need no thought control");
+//
+//        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+//
+//        System.out.println("In windows, the main drive is usually C:\\");
+//
+//        System.out.println("I can do backslashes \\, double backslashes \\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!");
+
+//    }
+//}
